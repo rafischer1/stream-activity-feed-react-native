dfac4ac944242e2783f63250c4cf9684ddc0fdb6
Robert Fischer <artiefischer@gmail.com>
2019-06-03 11:15:11 -0600

    init phase with login - add serverless handler

---
+++ screens/Feed.js
@@ -0,0 +1,30 @@
+import React from "react";
+import SafeAreaView from "react-native-safe-area-view";
+import {
+  StreamApp,
+  FlatFeed,
+  Activity,
+  LikeButton,
+  StatusUpdateForm
+} from "expo-activity-feed";
+
+const CustomActivity = props => {
+  return <Activity {...props} Footer={<LikeButton {...props} />} />;
+};
+
+const Feed = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }} forceInset={{ top: "always" }}>
+      <StreamApp
+        apiKey="tz7ht7p9pf8p"
+        appId="53044"
+        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidXNlci1vbmUifQ.LjTWUNiUbwrIF60zkeAtayT9pLfbO-GBBT0oAVoUFvI"
+      >
+        <FlatFeed Activity={CustomActivity} notify />
+        <StatusUpdateForm feedGroup="timeline" />
+      </StreamApp>
+    </SafeAreaView>
+  );
+};
+
+export default Feed;
